040792a8228d2f6797dd4d5a1ca128c4c86d2382
gabrielestacio <gabrielestaciosp@gmail.com>
2026-03-23 08:39:55 -0300

    update: basic editions

--- app/src/main/java/com/caio/caiocardozo/partidabasquete_kotlin/MainActivity.kt
+++ app/src/main/java/com/caio/caiocardozo/partidabasquete_kotlin/MainActivity.kt
@@ -14,9 +14,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pTimeA: TextView
     private lateinit var pTimeB: TextView
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         pTimeA = findViewById(R.id.placarTimeA)
@@ -25,45 +25,67 @@ class MainActivity : AppCompatActivity() {
         val bTresPontosTimeA: Button = findViewById(R.id.tresPontosA)
         val bDoisPontosTimeA: Button = findViewById(R.id.doisPontosA)
         val bTLivreTimeA: Button = findViewById(R.id.tiroLivreA)
+
         val bTresPontosTimeB: Button = findViewById(R.id.tresPontosB)
         val bDoisPontosTimeB: Button = findViewById(R.id.doisPontosB)
         val bTLivreTimeB: Button = findViewById(R.id.tiroLivreB)
+
         val bReiniciar: Button = findViewById(R.id.reiniciarPartida)
 
+        bTresPontosTimeA.setOnClickListener {
+            adicionarPontos(3, "A")
+        }
 
-        bTresPontosTimeA.setOnClickListener { adicionarPontos(3, "A") }
+        bDoisPontosTimeA.setOnClickListener {
+            adicionarPontos(2, "A")
+        }
 
-        bDoisPontosTimeA.setOnClickListener { adicionarPontos(2, "A") }
+        bTLivreTimeA.setOnClickListener {
+            adicionarPontos(1, "A")
+        }
 
-        bTLivreTimeA.setOnClickListener { adicionarPontos(1, "A") }
+        bTresPontosTimeB.setOnClickListener {
+            adicionarPontos(3, "B")
+        }
 
-        bTresPontosTimeB.setOnClickListener { adicionarPontos(3, "B") }
+        bDoisPontosTimeB.setOnClickListener {
+            adicionarPontos(2, "B")
+        }
 
-        bDoisPontosTimeB.setOnClickListener { adicionarPontos(2, "B") }
+        bTLivreTimeB.setOnClickListener {
+            adicionarPontos(1, "B")
+        }
 
-        bTLivreTimeB.setOnClickListener { adicionarPontos(1, "B") }
-
-        bReiniciar.setOnClickListener { reiniciarPartida() }
-
-
+        bReiniciar.setOnClickListener {
+            reiniciarPartida()
+        }
     }
 
     fun adicionarPontos(pontos: Int, time: String) {
-        if(time == "A"){
+        if (time == "A") {
             pontuacaoTimeA += pontos
-
-        }else {
+        } else {
             pontuacaoTimeB += pontos
-
         }
-        atualizaPlacar(time)
+        atualizarPlacar(time)
     }
 
-    fun atualizaPlacar(time: String){
-        if(time == "A"){
-            pTimeA.setText(pontuacaoTimeA.toString())
-        }else {
-            pTimeB.setText(pontuacaoTimeB.toString())
+    fun atualizarPlacar(time: String) {
+        if (time == "A") {
+            pTimeA.text = pontuacaoTimeA.toString()
+        } else {
+            pTimeB.text = pontuacaoTimeB.toString()
+        }
+
+        if (pontuacaoTimeA > pontuacaoTimeB) {
+            pTimeA.setTextColor(resources.getColor(R.color.purple_500))
+            pTimeB.setTextColor(resources.getColor(android.R.color.white))
+        } else if (pontuacaoTimeB > pontuacaoTimeA) {
+            pTimeB.setTextColor(resources.getColor(R.color.yellow_lakers))
+            pTimeA.setTextColor(resources.getColor(android.R.color.white))
+        } else {
+            pTimeA.setTextColor(resources.getColor(android.R.color.white))
+            pTimeB.setTextColor(resources.getColor(android.R.color.white))
         }
     }
 
@@ -72,7 +94,9 @@ class MainActivity : AppCompatActivity() {
         pTimeA.setText(pontuacaoTimeA.toString())
         pontuacaoTimeB = 0
         pTimeB.setText(pontuacaoTimeB.toString())
-        Toast.makeText(this,"Placar reiniciado",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Placar Reiniciado", Toast.LENGTH_SHORT).show()
 
+        pTimeA.setTextColor(resources.getColor(android.R.color.white))
+        pTimeB.setTextColor(resources.getColor(android.R.color.white))
     }
 }
